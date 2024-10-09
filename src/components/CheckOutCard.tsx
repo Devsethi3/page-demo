bbe801d2e05a0cef7e9b2d3dc4410367e74d700e
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-10-09 21:43:24 +0530

    Update CheckOutCard.tsx

--- src/components/CheckOutCard.tsx
+++ src/components/CheckOutCard.tsx
@@ -14,7 +14,7 @@ import { cn } from "../lib/utils";
 import { Calendar } from "./ui/calendar";
 import CalendarPopover from "./CalendarPopover";
 
-// Define
+// 
 interface DateRange {
   from: Date | undefined;
   to: Date | undefined;
