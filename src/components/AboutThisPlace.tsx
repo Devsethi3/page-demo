47278e52e1f3aa9ff8aaf9caa4f7b698d7aa18f3
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-10-31 22:20:42 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -32,7 +32,7 @@ const AboutThisPlace = () => {
               <p>
                 This sizable cozy home gazes at the Pacific Ocean and is just a
                 stone throw away from the local sandy beach. The hypnotic sounds
-                of the ocean waves crashing, the salty beach air, a curious sea
+                of the waves crashing, the salty beach air, a curious sea
                 otter poking his head out of the water, this and more all
                 observed from beautiful.
               </p>
