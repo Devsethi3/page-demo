2bdc7db75fa9c0572203133079242a43dcfd04a7
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-02 21:23:31 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -33,7 +33,7 @@ const AboutThisPlace = () => {
                 This sizable cozy home gazes at the Pacific Ocean and is just a
                 stone throw away from the local sandy beach. The hypnotic sounds
                 of the waves crashing, the salty beach air, a sea
-                otter poking his head of water, this and more all
+                otter poking his head of water, this and all
                 observed beautiful.
               </p>
               <p className="mt-4">
