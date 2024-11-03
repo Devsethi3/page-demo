3ceb245451acb1b9f784a67dd405ea1ad046a1ce
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-03 22:24:08 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -26,7 +26,7 @@ const AboutThisPlace = () => {
             </DialogHeader>
             <ScrollArea className="max-h-[500px]">
               This home is nestled in Newport and just
-              Historical, bay front shopping and adored,
+              Historical, bay front shopping and,
               all of this and more waiting for you.
               <p className="font-semibold mt-4">The space</p>{" "}
               <p>
