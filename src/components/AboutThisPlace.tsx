6ea3ddbc070a7a63b25b846143bbae61dedb7550
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-12 22:08:09 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -40,7 +40,7 @@ const AboutThisPlace = () => {
                 Three of the and
                 beautiful. Snuggling the fireplace and
                 gazing through spacious windows that offer you an inspiring
-                panoramic of the ocean and luck you may a
+                panoramic of the ocean and luck you may
                  of the majestic pod of Gray Whales that live in this
                 area.
               </p>
