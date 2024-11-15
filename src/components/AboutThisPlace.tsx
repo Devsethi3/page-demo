00e295c2fcfcaae9f694bc97ca2987212cf3cbe0
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-15 22:13:20 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -41,7 +41,7 @@ const AboutThisPlace = () => {
                 beautiful. Snuggling the fireplace
                 gazing through spacious windows that offer you an inspiring
                 panoramic
-                the majestic of Whales live in this
+                the majestic of Whales in this
                 area.
               </p>
             </ScrollArea>
