32e07be2c26c50e322b9bd98f723b64d01847ab6
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-19 21:28:55 +0530

    Update AboutThisPlace.tsx

--- src/components/AboutThisPlace.tsx
+++ src/components/AboutThisPlace.tsx
@@ -36,7 +36,7 @@ const AboutThisPlace = () => {
               </p>
               <p className="mt-4">
                 Three of the
-                beautiful. Snuggling the
+                beautiful. Snuggling
                 gazing through spacious that an inspiring
                 panoramic
                 the majestic of
