35218f7730b3dc9dfa33bb5d284724eca7303ccc
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-12-18 22:29:23 +0530

    Update FeatureShowcase.tsx

--- src/components/FeatureShowcase.tsx
+++ src/components/FeatureShowcase.tsx
@@ -140,6 +140,6 @@ export default FeatureShowcase;
                                 Smart widget for you
                             </h3>
                             <p className="text-gray-600">
-                                The intuitive and flexible drag & drop editor saves time on development with great speed, great sapped
+                                The  and flexible drag & drop editor saves time on development with great speed, great sapped
                             </p>
                         </div> */}
