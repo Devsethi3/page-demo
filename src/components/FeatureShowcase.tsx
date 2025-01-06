4b73747398dd2d09ac7c4cfb170c8797a6924d96
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-06 23:08:15 +0530

    Update FeatureShowcase.tsx

--- src/components/FeatureShowcase.tsx
+++ src/components/FeatureShowcase.tsx
@@ -128,7 +128,7 @@ const FeatureShowcase = () => {
 
 export default FeatureShowcase;
 
-{/* <div className="mb-8 w-full overflow-hidden">
+{/* <div className="mb-8 w-full overflow">
                             
                             
                                 
