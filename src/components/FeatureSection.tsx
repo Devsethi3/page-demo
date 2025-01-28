f826fd7818ede2cc20349b33c5cfe7b1db191854
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-28 23:32:12 +0530

    Update FeatureSection.tsx

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -28,7 +28,7 @@ const FeatureSection = () => {
                         </div>
                         <h3 className="mb-2 text-xl font-bold">Make informed business decision</h3>
                         <p className="text-gray-500">
-                            Our software provides comprehensive analytics, empowering you to identify trends, track
+                            Our software provides comprehensive analytics, empowering you to identify trends,
                             performance.
                         </p>
                     </div>
