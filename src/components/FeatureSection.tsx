82628054f3892a0d5821dd9b592b715a60fb2a1d
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-31 23:20:32 +0530

    Update FeatureSection.tsx

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -37,7 +37,7 @@ const FeatureSection = () => {
                         </div>
                         <h3 className="mb-2 text-xl font-bold">Scale your startup</h3>
                         <p className="text-gray-500">
-                            Our software is designed to grow alongside your business.
+                            Our software is designed to grow alongside your.
                         </p>
                     </div>
                 </div>
